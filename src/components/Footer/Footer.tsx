2551ef56c901e24b0e08797140ffe9833d1d549d
Fernando <fmoleromartin@gmail.com>
2023-11-03 00:46:21 +0100

    header

---
+++ src/components/Footer/Footer.tsx
@@ -0,0 +1,11 @@
+import "./Footer.scss";
+
+const Footer = () => {
+  return (
+    <div className="footer__container">
+      <p>DeCatLong Est.2023</p>
+    </div>
+  );
+};
+
+export default Footer;
