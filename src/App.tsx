220dafed70b13b67967f87808b05338d075ddf9b
abj1389 <abj_13@hotmail.com>
2023-11-03 03:04:25 +0100

    building sign-up-form component

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import "./App.scss";
+import SignUpForm from "./components/SignUpForm/SignUpForm";
 
 function App() {
   return (
     <>
       <h1>Hello World!</h1>
+      <SignUpForm />
     </>
   );
 }
