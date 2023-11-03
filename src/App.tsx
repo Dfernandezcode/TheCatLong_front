3c125aaa5892558b3b2f90aac70bcbef1d0cb04d
Daniel Fernandez <dfernandezcode@gmail.com>
2023-11-03 01:54:10 +0100

    card description and card preview added, colors to be added, ratings to be added, dropdown to be added

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,13 @@
 import "./App.scss";
+import CardDescription from "./components/card/card-description/card-description";
+import CardPreview from "./components/card/card-preview/card-preview";
 
 function App() {
   return (
     <>
       <h1>Hello World!</h1>
+      <CardPreview />
+      <CardDescription />
     </>
   );
 }
