e4282e29b8018efd8066131ba09bfcfcd9142801
abj1389 <abj_13@hotmail.com>
2023-11-07 13:26:29 +0100

    import of SignUpForm commented

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,5 @@
 import "./App.scss";
-import SignUpForm from "./components/SignUpForm/SignUpForm";
-
+// import SignUpForm from "./components/SignUpForm/SignUpForm";
 import CardDescription from "./components/card/card-description/card-description";
 import CardPreview from "./components/card/card-preview/card-preview";
 import { HashRouter, Routes, Route } from "react-router-dom";
