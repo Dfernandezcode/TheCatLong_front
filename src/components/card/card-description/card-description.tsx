32020f02d2b1bc80716a6a100c88a1b252af8dbb
Daniel Fernandez <dfernandezcode@gmail.com>
2023-11-14 17:12:41 +0100

    cards mock complete

--- src/components/card/card-description/card-description.tsx
+++ src/components/card/card-description/card-description.tsx
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import "./card-description.scss";
 import "../card-preview/card-preview.scss";
+import Rating from "../../rating/rating";
 
 interface ProductColorOption {
   colorName: string;
@@ -11,13 +12,14 @@ interface ProductDetails {
   name: string;
   price: number;
   colorOptions: ProductColorOption[];
+  rating: number;
 }
 
-interface CardPreviewProps {
+interface CardDescriptionProps {
   product: ProductDetails;
 }
 
-const CardDescription: React.FC<CardPreviewProps> = ({ product }) => {
+const CardDescription: React.FC<CardDescriptionProps> = ({ product }) => {
   const [currentImage, setCurrentImage] = useState<string>(product.colorOptions[0].imageUrl);
 
   const handleColorSelection = (imageUrl: string) => {
@@ -33,17 +35,22 @@ const CardDescription: React.FC<CardPreviewProps> = ({ product }) => {
             <h2 className="description__title__name">{product.name}</h2>
             <h2 className="description__title__name--secondary">{product.name}</h2>
           </div>
-          <p className="description__textarea__text">"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."</p>
-          <h3 className="description__textarea__price">€{product.price.toFixed(2)}</h3>
-          <button className="description__textarea__btn">Add to cart</button>
-          <ul className="preview__selector">
+
+          {/* Star rating display */}
+          <div className="description__rating">
+            {[...Array(5)].map((_, index) => (
+              <Rating key={index} filled={index < product.rating} />
+            ))}
+          </div>
+
+          <p className="description__textarea__text">"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."</p>
+          <ul className="description__selector">
             {product.colorOptions.map((option) => (
-              <li className="preview__selector--color" key={option.colorName} onClick={() => handleColorSelection(option.imageUrl)}>
-                {option.colorName.charAt(0).toUpperCase() + option.colorName.slice(1)}
-              </li>
+              <li className="description__selector--color" key={option.colorName} onClick={() => handleColorSelection(option.imageUrl)} style={{ backgroundColor: option.colorName }}></li>
             ))}
           </ul>
-          <div className="description__textarea__colors"></div>
+          <h3 className="description__textarea__price">€{product.price.toFixed(2)}</h3>
+          <button className="description__textarea__btn">Add to cart</button>
         </div>
       </div>
     </>
